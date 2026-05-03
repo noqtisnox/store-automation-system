c152e85adc37a02e3956363709429ec620df75bb
nox <cheporte.dev@gmail.com>
2026-05-03 19:41:46 +0300

    add graceful shutdown to hono

--- api/js-hono/src/index.ts
+++ api/js-hono/src/index.ts
@@ -11,8 +11,22 @@ app.get("/api/v1/health", (c) => {
   });
 });
 
-serve({ fetch: app.fetch, port: 3000 }, (info) => {
+const server = serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server running at http://localhost:${info.port}`);
 });
+
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
 
 export default app;
